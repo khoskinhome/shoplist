e725341d4755712436e677d5d6bed9feef2c8361
Karl Hoskin <karl@khoskin.co.uk>
2016-07-14 00:48:58 +0100

    removing old code.

--- install/Khaos-ShopList/lib/Khaos/ShopList.pm
+++ install/Khaos-ShopList/lib/Khaos/ShopList.pm
@@ -295,15 +295,6 @@ get '/list_all_items' => sub {
     };
 };
 
-sub get_shopping_list {
-
-    # TODO . this is a copy and paste get_items_n_shops_ordered()
-    # this needs to get the data from the shown/unshown items and also the quantities from the shopping_lists table.
-
-    my ($show_all) = @_;
-
-}
-
 sub get_items_n_shops_ordered {
     my ($show_all, $shopping_list_id) = @_;
 
@@ -426,77 +417,6 @@ sub get_items_n_shops_ordered {
     return $results;
 }
 
-#sub get_items_n_shops_ordered {
-#    my ($show_all) = @_;
-#
-#    my $where = '';
-#    $where = 'where i.show_item is true' if ! $show_all;
-#
-#    my $sql =<<"    EOSQL";
-#        select
-#            i.id as item_id,
-#            i.name as item_name,
-#            i.show_item,
-#            igs.id as item_group_id,
-#            igs.name as item_group_name,
-#            igs.tag  as item_group_tag,
-#            ishp.price as item_shop_price,
-#            s.name   as shop_name,
-#            s.tag    as shop_tag
-#        from items as i
-#            left join item_groups as igs  on (i.item_group_id = igs.id)
-#            left join item_shops  as ishp on (ishp.item_id = i.id )
-#            left join shops       as s    on (s.id = ishp.shop_id )
-#        $where
-#
-#        order by igs.sequence, i.name, s.name
-#
-#    EOSQL
-#
-#    my $sth = database->prepare($sql);
-#    $sth->execute();
-#
-#    my $results = [];
-#
-#    my $current_item_name  = '';
-#    my $current_item_group_name = '';
-#
-#    my $gen_item_shop = sub {
-#        my ($i_ar_last) = @_;
-#
-#        $i_ar_last->{shops_n_name} = "(".join(',',@{$i_ar_last->{shops}}).") ".$i_ar_last->{name};
-#    };
-#
-#    while ( my $row = $sth->fetchrow_hashref ){
-#        if ( $current_item_group_name ne $row->{item_group_name} ){
-#            $current_item_group_name = $row->{item_group_name};
-#            push @$results, {
-#                item_group_name => $row->{item_group_name},
-#                items => []
-#            };
-#        }
-#
-#        if ($current_item_name ne $row->{item_name}){
-#            $current_item_name = $row->{item_name};
-#            push @{$results->[$#$results]{items}}
-#                , { id    => $row->{item_id},
-#                    name  => $current_item_name,
-#                    shops => [ $row->{shop_tag}],
-#                  };
-#
-#            my $i_ar = $results->[$#$results]{items};
-#            $gen_item_shop->($i_ar->[$#$i_ar]);
-#
-#        } else {
-#            my $i_ar = $results->[$#$results]{items};
-#            push @{$i_ar->[$#$i_ar]{shops}}, $row->{shop_tag};
-#            $gen_item_shop->($i_ar->[$#$i_ar]);
-#        }
-#
-#    }
-#    return $results;
-#}
-
 sub _check_item_data {
     my ($data) = @_;
     die "need to supply an item name" if ! $data->{name};
@@ -517,7 +437,6 @@ sub _check_item_data {
         if ! $data->{item_group};
 
     return $shops;
-
 }
 
 sub add_item {
@@ -649,6 +568,5 @@ sub get_table {
 
     return $results;
 }
-
 
 true;
