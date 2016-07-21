11919ca3dcecd2521292563ea98422fe80cf1283
Karl Hoskin <karl@khoskin.co.uk>
2016-07-21 19:38:06 +0100

    bash alias minor mod

--- install/bin/shoplist-bash-aliases.sh
+++ install/bin/shoplist-bash-aliases.sh
@@ -1,3 +1,4 @@
 alias plackup.shoplist='cd /opt/shoplist/Khaos-ShopList/;plackup -r bin/app.psgi'
+alias nohup.plackup.shoplist='cd /opt/shoplist/Khaos-ShopList/;nohup plackup -r bin/app.psgi &'
 alias psql.shoplist='psql -U shoplist_write -h 127.0.0.1 -d shoplist'
 
